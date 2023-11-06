01a7585c28ee4fbdf52f03bee03827a101641d9b
Cliente CONTROL-PC <CltControl@GGIECLUGWRK195.espol.edu.ec>
2023-11-06 10:38:12 -0500

    Enviando

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -16,7 +16,7 @@ let cargarOpenMeteo = () => {
 
       //Etiquetas de los datos
       let data = responseJSON.hourly.temperature_2m;
-      let data2= responseJSON.hourly.precipitation_probability;
+      let data2 = responseJSON.hourly.precipitation_probability;
 
       //Objeto de configuración del gráfico
       let config = {
@@ -32,9 +32,7 @@ let cargarOpenMeteo = () => {
         },
       };
 
-
       let chart1 = new Chart(plotRef1, config);
-
 
       config = {
         type: "line",
@@ -49,7 +47,7 @@ let cargarOpenMeteo = () => {
         },
       };
 
-      let chart2= new Chart(plotRef2, config);
+      let chart2 = new Chart(plotRef2, config);
 
       console.log(responseJSON);
     })
@@ -57,3 +55,91 @@ let cargarOpenMeteo = () => {
 };
 
 cargarOpenMeteo();
+
+let parseXML = (responseText) => {
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+
+  // Referencia al elemento `#forecastbody` del documento HTML
+
+  let forecastElement = document.querySelector("#forecastbody");
+  forecastElement.innerHTML = "";
+
+  // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+  let timeArr = xml.querySelectorAll("time");
+
+  timeArr.forEach((time) => {
+    let from = time.getAttribute("from").replace("T", " ");
+
+    let humidity = time.querySelector("humidity").getAttribute("value");
+    let windSpeed = time.querySelector("windSpeed").getAttribute("mps");
+    let precipitation = time.querySelector("precipitation").getAttribute("probability");
+    let pressure =time.querySelector("pressure").getAttribute("value");
+    let cloud = time.querySelector("clouds").getAttribute("value");
+
+    let template = `
+            <tr>
+                <td>${from}</td>
+                <td>${humidity}</td>
+                <td>${windSpeed}</td>
+                <td>${precipitation}</td>
+                <td>${pressure}</td>
+                <td>${cloud}</td>
+            </tr>
+        `;
+
+    //Renderizando la plantilla en el elemento HTML
+    forecastElement.innerHTML += template;
+  });
+};
+
+
+
+
+//Callback
+let selectListener = async (event) => {
+
+
+  let selectedCity = event.target.value;
+  // Lea la entrada de almacenamiento local
+  let cityStorage = localStorage.getItem(selectedCity);
+
+  if (cityStorage == null) {
+    try {
+      //API key
+      let APIkey = "6b0de6154a69073662610d2e4e40bb30";
+      let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`;
+
+      let response = await fetch(url);
+      let responseText = await response.text();
+
+      await parseXML(responseText);
+      // Guarde la entrada de almacenamiento local
+      await localStorage.setItem(selectedCity, responseText);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    // Procese un valor previo
+    parseXML(cityStorage);
+  }
+};
+
+
+
+
+
+
+
+
+let loadForecastByCity = () => {
+  //Handling event
+  let selectElement = document.querySelector("select");
+  selectElement.addEventListener("change", selectListener);
+};
+
+
+
+
+
+loadForecastByCity();
